e2a756a02f345b952c65a43b0d7f8b5ed0dc35cf
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-18 12:34:46 +0000

    Update datasets.py

--- proof_of_concept_experiment/datasets.py
+++ proof_of_concept_experiment/datasets.py
@@ -1,1 +1,18 @@
-
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+def choose_dataset(select_dataset):
+    if select_dataset == "mnist":
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+        MNIST = torchvision.datasets.MNIST('data-cifar10', train=True, download=True, transform=transform)
+        return MNIST
+    elif select_dataset == "mnist2":
+        transform = transforms.Compose([transforms.Grayscale(3), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        MNIST = torchvision.datasets.MNIST('data-cifar10', train=True, download=True, transform=transform)
+        return MNIST
+    elif select_dataset == "cifar10":
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        CIFAR10 = torchvision.datasets.CIFAR10('data-cifar10', train=True, download=True, transform=transform)
+        return CIFAR10
+    else:
+        raise ValueError("Unknown dataset.")

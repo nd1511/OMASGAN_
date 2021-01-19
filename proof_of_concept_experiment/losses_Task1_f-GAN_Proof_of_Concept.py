906a74987fe7cfe48e95f92e9fc1a89f8eadd140
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-19 09:36:47 +0000

    Update losses_Task1_f-GAN_Proof_of_Concept.py

--- proof_of_concept_experiment/losses_Task1_f-GAN_Proof_of_Concept.py
+++ proof_of_concept_experiment/losses_Task1_f-GAN_Proof_of_Concept.py
@@ -67,3 +67,5 @@ class FGANLearningObjective(nn.Module):
             gradient_penalty = self.gammahalf * grad_pd_norm2.mean()
             loss_disc += gradient_penalty
         return loss_gen, loss_disc
+# Acknowledgement: Thanks to the repositories: [PyTorch-Template](https://github.com/victoresque/pytorch-template "PyTorch Template"), [Generative Models](https://github.com/shayneobrien/generative-models/blob/master/src/f_gan.py), [f-GAN](https://github.com/nowozin/mlss2018-madrid-gan), and [KLWGAN](https://github.com/ermongroup/f-wgan/tree/master/image_generation)
+# Also, thanks to the repositories: [Negative-Data-Augmentation](https://anonymous.4open.science/r/99219ca9-ff6a-49e5-a525-c954080de8a7/), [Negative-Data-Augmentation-Paper](https://openreview.net/forum?id=Ovp8dvB8IBH), and [BigGAN](https://github.com/ajbrock/BigGAN-PyTorch)

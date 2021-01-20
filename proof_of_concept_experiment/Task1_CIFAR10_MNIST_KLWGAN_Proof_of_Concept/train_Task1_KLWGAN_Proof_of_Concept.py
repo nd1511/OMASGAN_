559cffd7503f862b5d68437b0667fe725ee59fff
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-20 15:44:41 +0000

    Update train_Task1_KLWGAN_Proof_of_Concept.py

--- proof_of_concept_experiment/Task1_CIFAR10_MNIST_KLWGAN_Proof_of_Concept/train_Task1_KLWGAN_Proof_of_Concept.py
+++ proof_of_concept_experiment/Task1_CIFAR10_MNIST_KLWGAN_Proof_of_Concept/train_Task1_KLWGAN_Proof_of_Concept.py
@@ -1,4 +1,5 @@
 # Use: python train_Task1_KLWGAN_Proof_of_Concept.py --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ./data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed 2 --which_best FID --model BigGAN --experiment_name C10Ukl5
+# Acknowledgement: Thanks to the repository: [KLWGAN](https://github.com/ermongroup/f-wgan/tree/master/image_generation)
 # Acknowledgement: Thanks to the repositories: [PyTorch-Template](https://github.com/victoresque/pytorch-template "PyTorch Template"), [Generative Models](https://github.com/shayneobrien/generative-models/blob/master/src/f_gan.py), [f-GAN](https://github.com/nowozin/mlss2018-madrid-gan), and [KLWGAN](https://github.com/ermongroup/f-wgan/tree/master/image_generation)
 # Also, thanks to the repositories: [Negative-Data-Augmentation](https://anonymous.4open.science/r/99219ca9-ff6a-49e5-a525-c954080de8a7/), [Negative-Data-Augmentation-Paper](https://openreview.net/forum?id=Ovp8dvB8IBH), and [BigGAN](https://github.com/ajbrock/BigGAN-PyTorch)
 from __future__ import print_function
@@ -59,13 +60,10 @@ def run(config):
     if config['D_fp16']:
         D = D.half()
     GD = model.G_D(G, D, config['conditional'])
-    print('Number of params in G: {} D: {}'.format(
-        *[sum([p.data.nelement() for p in net.parameters()]) for net in [G, D]]))
-    state_dict = {'itr': 0, 'epoch': 0, 'save_num': 0, 'save_best_num': 0,
-                  'best_IS': 0, 'best_FID': 999999, 'config': config}
+    print('Number of params in G: {} D: {}'.format(*[sum([p.data.nelement() for p in net.parameters()]) for net in [G, D]]))
+    state_dict = {'itr': 0, 'epoch': 0, 'save_num': 0, 'save_best_num': 0, 'best_IS': 0, 'best_FID': 999999, 'config': config}
     if config['resume']:
-        utils_Task1_KLWGAN_Proof_of_Concept.load_weights(G, D, state_dict, config['weights_root'], experiment_name,
-                           config['load_weights'] if config['load_weights'] else None, G_ema if config['ema'] else None)
+        utils_Task1_KLWGAN_Proof_of_Concept.load_weights(G, D, state_dict, config['weights_root'], experiment_name, config['load_weights'] if config['load_weights'] else None, G_ema if config['ema'] else None)
     if config['parallel']:
         GD = nn.DataParallel(GD)
         if config['cross_replica']:
@@ -168,5 +166,6 @@ def main():
     run(config)
 if __name__ == '__main__':
     main()
+# Acknowledgement: Thanks to the repository: [KLWGAN](https://github.com/ermongroup/f-wgan/tree/master/image_generation)
 # Acknowledgement: Thanks to the repositories: [PyTorch-Template](https://github.com/victoresque/pytorch-template "PyTorch Template"), [Generative Models](https://github.com/shayneobrien/generative-models/blob/master/src/f_gan.py), [f-GAN](https://github.com/nowozin/mlss2018-madrid-gan), and [KLWGAN](https://github.com/ermongroup/f-wgan/tree/master/image_generation)
 # Also, thanks to the repositories: [Negative-Data-Augmentation](https://anonymous.4open.science/r/99219ca9-ff6a-49e5-a525-c954080de8a7/), [Negative-Data-Augmentation-Paper](https://openreview.net/forum?id=Ovp8dvB8IBH), and [BigGAN](https://github.com/ajbrock/BigGAN-PyTorch)

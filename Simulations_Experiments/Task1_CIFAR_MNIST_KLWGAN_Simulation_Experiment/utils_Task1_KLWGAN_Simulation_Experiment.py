59d33e5cf7088eb452fb7960d1fb878aa9822f9a
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-21 17:55:49 +0000

    Update utils_Task1_KLWGAN_Simulation_Experiment.py

--- Simulations_Experiments/Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/utils_Task1_KLWGAN_Simulation_Experiment.py
+++ Simulations_Experiments/Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/utils_Task1_KLWGAN_Simulation_Experiment.py
@@ -1,7 +1,7 @@
+from __future__ import print_function
 # Acknowledgement: Thanks to the repository: [KLWGAN](https://github.com/ermongroup/f-wgan/tree/master/image_generation)
 # Acknowledgement: Thanks to the repositories: [PyTorch-Template](https://github.com/victoresque/pytorch-template "PyTorch Template"), [Generative Models](https://github.com/shayneobrien/generative-models/blob/master/src/f_gan.py), [f-GAN](https://github.com/nowozin/mlss2018-madrid-gan), and [KLWGAN](https://github.com/ermongroup/f-wgan/tree/master/image_generation)
 # Also, thanks to the repositories: [Negative-Data-Augmentation](https://anonymous.4open.science/r/99219ca9-ff6a-49e5-a525-c954080de8a7/), [Negative-Data-Augmentation-Paper](https://openreview.net/forum?id=Ovp8dvB8IBH), and [BigGAN](https://github.com/ajbrock/BigGAN-PyTorch)
-from __future__ import print_function
 from torch.optim.optimizer import Optimizer
 import math
 import sys

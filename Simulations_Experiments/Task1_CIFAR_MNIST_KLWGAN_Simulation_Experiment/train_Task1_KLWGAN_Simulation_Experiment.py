49aa35b188aeb4d7817a38578f19e603131b90a6
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-21 18:14:55 +0000

    Update train_Task1_KLWGAN_Simulation_Experiment.py

--- Simulations_Experiments/Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/train_Task1_KLWGAN_Simulation_Experiment.py
+++ Simulations_Experiments/Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/train_Task1_KLWGAN_Simulation_Experiment.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 # Also, thanks to the repositories: [Negative-Data-Augmentation](https://anonymous.4open.science/r/99219ca9-ff6a-49e5-a525-c954080de8a7/), [Negative-Data-Augmentation-Paper](https://openreview.net/forum?id=Ovp8dvB8IBH), and [BigGAN](https://github.com/ajbrock/BigGAN-PyTorch)
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 #select_dataset = "select_dataset"
 select_dataset = "cifar10"
 #select_dataset = "mnist"
@@ -37,20 +37,19 @@ def run(config):
         config['skip_init'] = True
     config = update_config_roots(config)
     device = 'cuda'
-    utils_Task1_KLWGAN_Proof_of_Concept.seed_rng(config['seed'])
-    utils_Task1_KLWGAN_Proof_of_Concept.prepare_root(config)
+    utils_Task1_KLWGAN_Simulation_Experiment.seed_rng(config['seed'])
+    utils_Task1_KLWGAN_Simulation_Experiment.prepare_root(config)
     torch.backends.cudnn.benchmark = True
     model = __import__(config['model'])
     experiment_name = (config['experiment_name'] if config['experiment_name']
-                       else utils_Task1_KLWGAN_Proof_of_Concept.name_from_config(config))
+                       else utils_Task1_KLWGAN_Simulation_Experiment.name_from_config(config))
     print('Experiment name is %s' % experiment_name)
     G = model.Generator(**config).to(device)
     D = model.Discriminator(**config).to(device)
     if config['ema']:
-        print('Preparing EMA for G with decay of {}'.format(
-            config['ema_decay']))
+        print('Preparing EMA for G with decay of {}'.format(config['ema_decay']))
         G_ema = model.Generator(**{**config, 'skip_init': True, 'no_optim': True}).to(device)
-        ema = utils_Task1_KLWGAN_Proof_of_Concept.ema(G, G_ema, config['ema_decay'], config['ema_start'])
+        ema = utils_Task1_KLWGAN_Simulation_Experiment.ema(G, G_ema, config['ema_decay'], config['ema_start'])
     else:
         G_ema, ema = None, None
     if config['G_fp16']:
@@ -63,44 +62,35 @@ def run(config):
     print('Number of params in G: {} D: {}'.format(*[sum([p.data.nelement() for p in net.parameters()]) for net in [G, D]]))
     state_dict = {'itr': 0, 'epoch': 0, 'save_num': 0, 'save_best_num': 0, 'best_IS': 0, 'best_FID': 999999, 'config': config}
     if config['resume']:
-        utils_Task1_KLWGAN_Proof_of_Concept.load_weights(G, D, state_dict, config['weights_root'], experiment_name, config['load_weights'] if config['load_weights'] else None, G_ema if config['ema'] else None)
+        utils_Task1_KLWGAN_Simulation_Experiment.load_weights(G, D, state_dict, config['weights_root'], experiment_name, config['load_weights'] if config['load_weights'] else None, G_ema if config['ema'] else None)
     if config['parallel']:
         GD = nn.DataParallel(GD)
         if config['cross_replica']:
             patch_replication_callback(GD)
-    test_metrics_fname = '%s/%s_log.jsonl' % (config['logs_root'],
-                                              experiment_name)
+    test_metrics_fname = '%s/%s_log.jsonl' % (config['logs_root'], experiment_name)
     train_metrics_fname = '%s/%s' % (config['logs_root'], experiment_name)
     print('Inception Metrics will be saved to {}'.format(test_metrics_fname))
-    test_log = utils_Task1_KLWGAN_Proof_of_Concept.MetricsLogger(test_metrics_fname,
-                                   reinitialize=(not config['resume']))
+    test_log = utils_Task1_KLWGAN_Simulation_Experiment.MetricsLogger(test_metrics_fname, reinitialize=(not config['resume']))
     print('Training Metrics will be saved to {}'.format(train_metrics_fname))
-    train_log = utils_Task1_KLWGAN_Proof_of_Concept.MyLogger(train_metrics_fname,
-                               reinitialize=(not config['resume']),
-                               logstyle=config['logstyle'])
-    utils_Task1_KLWGAN_Proof_of_Concept.write_metadata(config['logs_root'],
-                         experiment_name, config, state_dict)
+    train_log = utils_Task1_KLWGAN_Simulation_Experiment.MyLogger(train_metrics_fname, reinitialize=(not config['resume']), logstyle=config['logstyle'])
+    utils_Task1_KLWGAN_Simulation_Experiment.write_metadata(config['logs_root'], experiment_name, config, state_dict)
     D_batch_size = (config['batch_size'] * config['num_D_steps']
                     * config['num_D_accumulations'])
-    loaders = utils_Task1_KLWGAN_Proof_of_Concept.get_data_loaders(**{**config, 'batch_size': D_batch_size,
+    loaders = utils_Task1_KLWGAN_Simulation_Experiment.get_data_loaders(**{**config, 'batch_size': D_batch_size,
                                         'start_itr': state_dict['itr']})
     G_batch_size = max(config['G_batch_size'], config['batch_size'])
-    z_, y_ = utils_Task1_KLWGAN_Proof_of_Concept.prepare_z_y(G_batch_size, G.dim_z, config['n_classes'],
-                               device=device, fp16=config['G_fp16'])
-    fixed_z, fixed_y = utils_Task1_KLWGAN_Proof_of_Concept.prepare_z_y(G_batch_size, G.dim_z,
-                                         config['n_classes'], device=device,
-                                         fp16=config['G_fp16'])
+    z_, y_ = utils_Task1_KLWGAN_Simulation_Experiment.prepare_z_y(G_batch_size, G.dim_z, config['n_classes'], device=device, fp16=config['G_fp16'])
+    fixed_z, fixed_y = utils_Task1_KLWGAN_Simulation_Experiment.prepare_z_y(G_batch_size, G.dim_z, config['n_classes'], device=device, fp16=config['G_fp16'])
     fixed_z.sample_()
     fixed_y.sample_()
     if not config['conditional']:
         fixed_y.zero_()
         y_.zero_()
     if config['which_train_fn'] == 'GAN':
-        train = train_fns.GAN_training_function(G, D, GD, z_, y_,
-                                                ema, state_dict, config)
+        train = train_fns.GAN_training_function(G, D, GD, z_, y_, ema, state_dict, config)
     else:
         train = train_fns.dummy_training_function()
-    sample = functools.partial(utils_Task1_KLWGAN_Proof_of_Concept.sample,
+    sample = functools.partial(utils_Task1_KLWGAN_Simulation_Experiment.sample,
                                G=(G_ema if config['ema'] and config['use_ema']
                                    else G),
                                z_=z_, y_=y_, config=config)
@@ -115,7 +105,7 @@ def run(config):
     print("the data moments is ", data_moments)
     for epoch in range(state_dict['epoch'], config['num_epochs']):
         if config['pbar'] == 'mine':
-            pbar = utils_Task1_KLWGAN_Proof_of_Concept.progress(
+            pbar = utils_Task1_KLWGAN_Simulation_Experiment.progress(
                 loaders[0], displaytype='s1k' if config['use_multiepoch_sampler'] else 'eta')
         else:
             pbar = tqdm(loaders[0])
@@ -133,7 +123,7 @@ def run(config):
             train_log.log(itr=int(state_dict['itr']), **metrics)
             if (config['sv_log_interval'] > 0) and (not (state_dict['itr'] % config['sv_log_interval'])):
                 train_log.log(itr=int(state_dict['itr']),
-                              **{**utils_Task1_KLWGAN_Proof_of_Concept.get_SVs(G, 'G'), **utils_Task1_KLWGAN_Proof_of_Concept.get_SVs(D, 'D')})
+                              **{**utils_Task1_KLWGAN_Simulation_Experiment.get_SVs(G, 'G'), **utils_Task1_KLWGAN_Simulation_Experiment.get_SVs(D, 'D')})
             if config['pbar'] == 'mine':
                 print(', '.join(['itr: %d' % state_dict['itr']]
                                 + ['%s : %+4.3f' % (key, metrics[key])
@@ -146,13 +136,13 @@ def run(config):
                         G_ema.eval()
                 train_fns.save_and_sample(G, D, G_ema, z_, y_, fixed_z, fixed_y, state_dict, config, experiment_name)
             experiment_name = (config['experiment_name'] if config['experiment_name']
-                               else utils_Task1_KLWGAN_Proof_of_Concept.name_from_config(config))
+                               else utils_Task1_KLWGAN_Simulation_Experiment.name_from_config(config))
             if (not (state_dict['itr'] % config['test_every'])) and (epoch >= config['start_eval']):
                 if config['G_eval_mode']:
                     G.eval()
                     if config['ema']:
                         G_ema.eval()
-                utils_Task1_KLWGAN_Proof_of_Concept.sample_inception(
+                utils_Task1_KLWGAN_Simulation_Experiment.sample_inception(
                     G_ema if config['ema'] and config['use_ema'] else G, config, str(epoch))
                 folder_number = str(epoch)
                 sample_moments = '%s/%s/%s/samples.npz' % (config['samples_root'], experiment_name, folder_number)
@@ -160,7 +150,7 @@ def run(config):
                 train_fns.update_FID(G, D, G_ema, state_dict, config, FID, experiment_name, test_log)
         state_dict['epoch'] += 1
 def main():
-    parser = utils_Task1_KLWGAN_Proof_of_Concept.prepare_parser()
+    parser = utils_Task1_KLWGAN_Simulation_Experiment.prepare_parser()
     config = vars(parser.parse_args())
     print(config)
     run(config)

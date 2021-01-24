5611ebefc7fcff484cbef0241f2f2fe741067418
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-24 18:27:39 +0000

    Create run_all_OMASGAN_Tasks.sh

---
+++ Experiments/run_all_OMASGAN_Tasks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Run simulation: Experiment for every individual data set
+# For every abnormal class
+# We choose the abnormal class to be 0.
+# The random seed is set to 2.
+echo "Run OMASGAN Task 1"
+cd ../Simulations_Experiments/
+python train_Task1_fGAN_Simulation_Experiment.py
+echo "Run Task 2"
+python train_Task2_fGAN_Simulation_Experiment.py
+echo "Run Task 3"
+python train_Task3_fGAN_Simulation_Experiment.py
+echo "Run Task 3 J"
+python train_Task3_J_fGAN_Simulation_Experiment.py
+exit 0
+#for m in $(seq 0 2)
+#do
+#    echo "Set the Random Seed: $m"
+#    for i in $(seq 0 9)
+#    do
+#        echo "Run Abnormal Digit: $i"
+#        python train.py --dataset mnist --isize 32 --nc 1 --niter 15 --abnormal_class $i #--manualseed $m
+#    done
+#done
+#exit 0

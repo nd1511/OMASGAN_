18046b55ae2dbfec59a6aedc40268af7635ffa16
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-24 10:01:49 +0000

    Update run_all_OMASGAN_Tasks.sh

--- Simulations_Experiments/run_all_OMASGAN_Tasks.sh
+++ Simulations_Experiments/run_all_OMASGAN_Tasks.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Run simulation: MNIST experiment for every individual data set.
-# For every abnormal MNIST digit
-# We choose the abnormal digit to be 0.
+# Run simulation: Experiment for every individual data set
+# For every abnormal class
+# We choose the abnormal class to be 0.
 # The random seed is set to 2.
-echo "Run MNIST, Task 1"
+echo "Run Task 1"
 cd ./Simulations_Experiments/
 python train_Task1_fGAN_Simulation_Experiment.py
-echo "Run MNIST, Task 2"
+echo "Run Task 2"
 python train_Task2_fGAN_Simulation_Experiment.py
-echo "Run MNIST, Task 3"
+echo "Run Task 3"
 python train_Task3_fGAN_Simulation_Experiment.py
-echo "Run MNIST, Task 3 J"
+echo "Run Task 3 J"
 python train_Task3_J_fGAN_Simulation_Experiment.py
 exit 0
 #for m in $(seq 0 2)

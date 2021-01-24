4c343d0edd99719ecd4369ac1f2eb0392909c2dd
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-24 10:02:53 +0000

    Update run_all_OMASGAN_Tasks.sh

--- Simulations_Experiments/run_all_OMASGAN_Tasks.sh
+++ Simulations_Experiments/run_all_OMASGAN_Tasks.sh
@@ -4,7 +4,7 @@
 # We choose the abnormal class to be 0.
 # The random seed is set to 2.
 echo "Run Task 1"
-cd ./Simulations_Experiments/
+#cd ./Simulations_Experiments/
 python train_Task1_fGAN_Simulation_Experiment.py
 echo "Run Task 2"
 python train_Task2_fGAN_Simulation_Experiment.py

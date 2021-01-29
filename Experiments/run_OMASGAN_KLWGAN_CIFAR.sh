d2f317b87d9fc8a5bbf333974fdf0da08ee41bd2
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-01-29 11:57:21 +0000

    Update run_OMASGAN_KLWGAN_CIFAR.sh

--- Experiments/run_OMASGAN_KLWGAN_CIFAR.sh
+++ Experiments/run_OMASGAN_KLWGAN_CIFAR.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 # Simulation: Run experiment for every individual image data set
-cd ../Simulations_Experiments/Task1_CIFAR10_MNIST_KLWGAN_Simulation_Experiment/
-python train_Task1_KLWGAN_Simulation_Experiment.py --select_dataset cifar10 --abnormal_class 0 --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ./data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed 2 --which_best FID --model BigGAN --experiment_name C10Ukl5
-cd ../Task2_CIFAR10_MNIST_KLWGAN_Simulation_Experiment/
-# Use: ../Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/data/
-python train.py --select_dataset cifar10 --abnormal_class 0 --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ../Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed 2 --which_best FID --model BigGAN --experiment_name C10Ukl5
-cd ../Task3_CIFAR10_MNIST_KLWGAN_Simulation_Experiment/
-python train.py --select_dataset cifar10 --abnormal_class 0 --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ../Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed 2 --which_best FID --model BigGAN --experiment_name C10Ukl5
-cd ../Task3_J_CIFAR10_MNIST_KLWGAN_Simulation_Experiment/
-python train.py --select_dataset cifar10 --abnormal_class 0 --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ../Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed 2 --which_best FID --model BigGAN --experiment_name C10Ukl5
+for i in $(seq 0 9)
+do
+  # Manual Seed: $m
+  for m in $(seq 0 2)
+  do
+    # Seed: $m
+    echo "Run OMASGAN Task 1"
+    cd ../Simulations_Experiments/Task1_CIFAR10_MNIST_KLWGAN_Simulation_Experiment/
+    python train_Task1_KLWGAN_Simulation_Experiment.py --select_dataset cifar10 --abnormal_class $i --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ./data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed $m --which_best FID --model BigGAN --experiment_name C10Ukl5
+    echo "Run Task 2"
+    cd ../Task2_CIFAR10_MNIST_KLWGAN_Simulation_Experiment/
+    # Use: ../Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/data/
+    python train.py --select_dataset cifar10 --abnormal_class $i --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ../Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed $m --which_best FID --model BigGAN --experiment_name C10Ukl5
+    echo "Run Task 3"
+    cd ../Task3_CIFAR10_MNIST_KLWGAN_Simulation_Experiment/
+    python train.py --select_dataset cifar10 --abnormal_class $i --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ../Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed $m --which_best FID --model BigGAN --experiment_name C10Ukl5
+    echo "Run Task 3 J"
+    cd ../Task3_J_CIFAR10_MNIST_KLWGAN_Simulation_Experiment/
+    python train.py --select_dataset cifar10 --abnormal_class $i --shuffle --batch_size 64 --parallel --num_G_accumulations 1 --num_D_accumulations 1 --num_epochs 500 --num_D_steps 4 --G_lr 2e-4 --D_lr 2e-4 --dataset C10 --data_root ../Task1_CIFAR_MNIST_KLWGAN_Simulation_Experiment/data/ --G_ortho 0.0 --G_attn 0 --D_attn 0 --G_init N02 --D_init N02 --ema --use_ema --ema_start 1000 --start_eval 50 --test_every 5000 --save_every 2000 --num_best_copies 5 --num_save_copies 2 --loss_type kl_5 --seed $m --which_best FID --model BigGAN --experiment_name C10Ukl5
+  done
+done
+exit 0

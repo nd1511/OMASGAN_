2e6d7520742a5351316acc17e85a7790d44c2c4a
Anonymous-Author-2021 <77455141+Anonymous-Author-2021@users.noreply.github.com>
2021-02-08 14:32:38 +0000

    Update train.py

--- Simulations_Experiments/Task3_J_CIFAR_MNIST_KLWGAN_Simulation_Experiment/train.py
+++ Simulations_Experiments/Task3_J_CIFAR_MNIST_KLWGAN_Simulation_Experiment/train.py
@@ -123,6 +123,9 @@ def run(config):
                 x, y = x.to(device).half(), y.to(device)
             else:
                 x, y = x.to(device), y.to(device)
+            print('')
+            # Random seed
+            print(config['seed'])
             metrics = train(x, y)
             # We double the learning rate if we double the batch size.
             train_log.log(itr=int(state_dict['itr']), **metrics)
